2c0fb99c43e7c3634a48f3544fae9e7e4e792754
Soumyadip Mondal <soumyadip.mondal@gmail.com>
2020-10-10 13:09:20 +0530

    universal and Ssr installed

--- spaceX/src/app/app.component.ts
+++ spaceX/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'spaceX';
+export class AppComponent implements OnInit {
+  title:string = '';
+
+  //implementing angular universal for Server Side Rendering.
+  constructor(
+    @Inject(PLATFORM_ID) private platform_id
+  ){}
+
+  ngOnInit(){
+    if(isPlatformBrowser(this.platform_id)){
+      this.title = 'Client spaceX';
+    }
+    console.log('hi');
+    
+    this.title = 'Server spaceX';
+  }
+
+  //npm run build:ssr and npm run serve:ssr
+
 }
